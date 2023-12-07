cb0ff933f03808f038078fec458a3dd8f5f616ae
Sofia d'Atri <datris@usi.ch>
2023-12-07 15:39:39 +0100

    Objects.h
    Refactored code.

--- Objects.h
+++ Objects.h
@@ -2,46 +2,46 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
-#include <cmath>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <vector>
-
 #include "Object.h"
 
-using namespace std;
-
 /**
- Implementation of the class Object for sphere shape.
+ * @brief Class representing a sphere object.
  */
 class Sphere : public Object {
 private:
-  float radius;     ///< Radius of the sphere
-  glm::vec3 center; ///< Center of the sphere
+  // Radius of the sphere
+  float radius = 1.0;
+
+  // Center of the sphere
+  glm::vec3 center = glm::vec3(0.0);
 
 public:
   /**
-   The constructor of the sphere
-   @param radius Radius of the sphere
-   @param center Center of the sphere
-   @param color Color of the sphere
+   * @brief Constructor for the sphere with a specified color.
+   * @param color Color of the sphere.
    */
-  Sphere(float radius, glm::vec3 center, glm::vec3 color)
-      : radius(radius), center(center) {
-    this->color = color;
-  }
-  Sphere(float radius, glm::vec3 center, Material material)
-      : radius(radius), center(center) {
-    this->material = material;
-  }
-  /** Implementation of the intersection function*/
+  Sphere(glm::vec3 color) { this->color = color; }
+
+  /**
+   * @brief Constructor for the sphere with a specified material.
+   * @param material Material of the sphere.
+   */
+  Sphere(Material material) { this->material = material; }
+
+  /**
+   * @brief Implementation of the intersection function.
+   * @param ray The ray to check for intersection.
+   * @return The Hit structure representing the intersection.
+   */
   Hit intersect(Ray ray) {
+    glm::vec3 newOrigin =
+        glm::vec3(inverseTransformationMatrix * glm::vec4(ray.origin, 1.0));
+    glm::vec3 newDirection = glm::normalize(
+        glm::vec3(inverseTransformationMatrix * glm::vec4(ray.direction, 0.0)));
 
-    glm::vec3 c = center - ray.origin;
-
+    glm::vec3 c = center - newOrigin;
     float cdotc = glm::dot(c, c);
-    float cdotd = glm::dot(c, ray.direction);
+    float cdotd = glm::dot(c, newDirection);
 
     Hit hit;
 
@@ -49,27 +49,32 @@ public:
     if (cdotc > cdotd * cdotd) {
       D = sqrt(cdotc - cdotd * cdotd);
     }
+
     if (D <= radius) {
       hit.hit = true;
       float t1 = cdotd - sqrt(radius * radius - D * D);
       float t2 = cdotd + sqrt(radius * radius - D * D);
 
-      float t = t1;
-      if (t < 0)
-        t = t2;
+      float t = (t1 < 0) ? t2 : t1;
       if (t < 0) {
         hit.hit = false;
         return hit;
       }
 
-      hit.intersection = ray.origin + t * ray.direction;
-      hit.normal = glm::normalize(hit.intersection - center);
+      glm::vec3 newIntersection = newOrigin + t * newDirection;
+      glm::vec3 newNormal = glm::normalize(newIntersection - center);
+
+      hit.intersection =
+          glm::vec3(transformationMatrix * glm::vec4(newIntersection, 1.0));
       hit.distance = glm::distance(ray.origin, hit.intersection);
       hit.object = this;
 
-      hit.uv.s = (asin(hit.normal.y) + M_PI / 2) / M_PI;
-      hit.uv.t = (atan2(hit.normal.z, hit.normal.x) + M_PI) / (2 * M_PI);
+      glm::vec3 newNormalGlobal =
+          glm::normalize(glm::vec3(normalMatrix * glm::vec4(newNormal, 0.0)));
+      hit.normal = newNormalGlobal;
 
+      hit.uv.s = (asin(newNormal.y) + M_PI / 2) / M_PI;
+      hit.uv.t = (atan2(newNormal.z, newNormal.x) + M_PI) / (2 * M_PI);
     } else {
       hit.hit = false;
     }
@@ -77,39 +82,63 @@ public:
   }
 };
 
+/**
+ * @brief Class representing a plane object.
+ */
 class Plane : public Object {
 
 private:
-  glm::vec3 normal;
-  glm::vec3 point;
+  // Normal vector of the plane.
+  glm::vec3 normal = glm::vec3(0.0f, 1.0f, 0.0f);
+
+  // A point on the plane.
+  glm::vec3 point = glm::vec3(0.0f); 
 
 public:
+  /**
+   * @brief Constructor for the plane with a specified point and normal.
+   * @param point A point on the plane.
+   * @param normal Normal vector of the plane.
+   */
   Plane(glm::vec3 point, glm::vec3 normal) : point(point), normal(normal) {}
+
+  /**
+   * @brief Constructor for the plane with a specified point, normal, and
+   * material.
+   * @param point A point on the plane.
+   * @param normal Normal vector of the plane.
+   * @param material Material of the plane.
+   */
   Plane(glm::vec3 point, glm::vec3 normal, Material material)
       : point(point), normal(normal) {
     this->material = material;
   }
-  Hit intersect(Ray ray) {
 
+  /**
+   * @brief Implementation of the intersection function for the plane.
+   * @param ray The ray to check for intersection.
+   * @return The Hit structure representing the intersection.
+   */
+  Hit intersect(Ray ray) {
     Hit hit;
     hit.hit = false;
 
     float ddotN = glm::dot(ray.direction, normal);
 
     if (ddotN == 0) {
-      // the ray is parallel to the plane
+      // The ray is parallel to the plane.
       return hit;
     }
 
     float podotN = glm::dot(point - ray.origin, normal);
     float t = podotN / ddotN;
+
     if (t < 0) {
       return hit;
     }
+
     hit.intersection = ray.origin + t * ray.direction;
-    // hit.normal = glm::normalize(hit.intersection);
     hit.normal = normal;
-    // hit.distance = glm::distance(ray.origin, hit.intersection);
     hit.distance = t;
     hit.object = this;
     hit.hit = true;
@@ -118,17 +147,29 @@ public:
   }
 };
 
-
+/**
+ * @brief Class representing a cone object.
+ */
 class Cone : public Object {
 private:
+  // Base of the cone represented as a plane.
   Plane *base;
 
 public:
+  /**
+   * @brief Constructor for the cone with a specified material.
+   * @param material Material of the cone.
+   */
   Cone(Material material) {
     this->material = material;
     base = new Plane(glm::vec3(0, 1, 0), glm::vec3(0, 1, 0), material);
   }
 
+  /**
+   * @brief Implementation of the intersection function for the cone.
+   * @param ray The ray to check for intersection.
+   * @return The Hit structure representing the intersection.
+   */
   Hit intersect(Ray ray) {
     Hit hit;
     hit.hit = false;
@@ -174,7 +215,7 @@ public:
     Hit basehit = base->intersect(rayy);
 
     if (basehit.hit && basehit.distance < t &&
-        length(basehit.intersection - glm::vec3(0, 1, 0)) <= 1.0) {
+        glm::length(basehit.intersection - glm::vec3(0, 1, 0)) <= 1.0) {
       newIntersection = basehit.intersection;
       newNormal = basehit.normal;
     }
