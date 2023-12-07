53ec5028927304c8d0a04abe7ee963356126bba4
Sofia d'Atri <datris@usi.ch>
2023-12-07 15:28:49 +0100

    Core.h
    Refactored Core.h

--- Core.h
+++ Core.h
@@ -5,26 +5,23 @@
 
 #include "glm/geometric.hpp"
 #include "glm/glm.hpp"
-#include <cmath>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <vector>
-
-using namespace std;
 
 /**
- Class representing a single ray.
+ * @brief Class representing a single ray.
  */
 class Ray {
 public:
-  glm::vec3 origin;    ///< Origin of the ray
-  glm::vec3 direction; ///< Direction of the ray
-                       /**
-                        Contructor of the ray
-                        @param origin Origin of the ray
-                        @param direction Direction of the ray
-                        */
+  // Origin of the ray.
+  glm::vec3 origin;
+
+  // Direction of the ray.
+  glm::vec3 direction;
+
+  /**
+   * @brief Constructor for the ray.
+   * @param origin Origin of the ray.
+   * @param direction Direction of the ray.
+   */
   Ray(glm::vec3 origin, glm::vec3 direction)
       : origin(origin), direction(direction) {}
 };
@@ -32,18 +29,28 @@ public:
 class Object;
 
 /**
- Structure representing the even of hitting an object
+ * @brief Structure representing the result of hitting an object.
  */
 struct Hit {
-  bool hit;         ///< Boolean indicating whether there was or there was no
-                    ///< intersection with an object
-  glm::vec3 normal; ///< Normal vector of the intersected object at the
-                    ///< intersection point
-  glm::vec3 intersection; ///< Point of Intersection
-  float distance; ///< Distance from the origin of the ray to the intersection
-                  ///< point
-  Object *object; ///< A pointer to the intersected object
-  glm::vec2 uv; ///< Coordinates for computing the texture (texture coordinates)
+  // Boolean indicating whether there was an intersection with an object.
+  bool hit;
+
+  // Normal vector of the intersected object at the intersection point.
+  glm::vec3 normal;
+
+  // Point of intersection.
+  glm::vec3 intersection;
+
+  // Distance from the origin of the ray to the intersection point.
+  float distance;
+
+  // A pointer to the intersected object.
+  Object *object;
+
+  // Coordinates for computing the texture (texture coordinates).
+  glm::vec2 uv;
 };
+
+// add bvh box
 
 #endif // CORE_H
