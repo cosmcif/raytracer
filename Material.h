e8bbc9853cf3848a5694fea89d95e97b2d8af85c
Sofia d'Atri <datris@usi.ch>
2023-12-09 17:13:51 +0100

    style: fixed clion problems
    Signed-off-by: Sofia d'Atri <datris@usi.ch>

--- Material.h
+++ Material.h
@@ -16,12 +16,12 @@ struct Material {
   glm::vec3 diffuse = glm::vec3(1.0);
   glm::vec3 specular = glm::vec3(0.0);
 
-  float reflection = 0.0f;
-  float refraction = 0.0f;
-  float refraction_index = 1.0f;
+//  float reflection = 0.0f;
+//  float refraction = 0.0f;
+//  float refraction_index = 1.0f;
 
   float shininess = 0.0;
-  glm::vec3 (*texture)(glm::vec2 uv) = NULL;
+  glm::vec3 (*texture)(glm::vec2 uv) = nullptr;
 };
 
 #endif /* Material_h */
