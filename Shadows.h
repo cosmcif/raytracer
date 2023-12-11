007ad1500be7e57bf0838dd886e931d9df21952b
Sofia d'Atri <datris@usi.ch>
2023-12-11 10:19:00 +0100

    feat: parallelization for shadows
    Signed-off-by: Sofia d'Atri <datris@usi.ch>

--- Shadows.h
+++ Shadows.h
@@ -4,9 +4,12 @@
 
 #ifndef RAYTRACER_SHADOWS_H
 #define RAYTRACER_SHADOWS_H
+
 #include "glm/glm.hpp"
 #include "glm/gtc/random.hpp"
 #include <vector>
+#include <omp.h>
+
 
 class SoftShadow {
 private:
@@ -18,7 +21,7 @@ public:
             : lightPosition(lightPosition) {
     }
 
-    static glm::vec3 generateRandomDirectionInHemisphere(const glm::vec3& normal) {
+    static glm::vec3 randomDirection(const glm::vec3 &normal, float m = 1.0f) {
         // Generate a random direction in the local tangent space
         glm::vec3 tangentX, tangentY;
         glm::vec3 tmp = glm::abs(normal.y) > 0.99f ? glm::vec3(1.0f, 0.0f, 0.0f) : glm::vec3(0.0f, 1.0f, 0.0f);
@@ -26,7 +29,7 @@ public:
         tangentY = glm::cross(normal, tangentX);
 
         // Generate random spherical coordinates
-        float theta = glm::acos(glm::sqrt(glm::linearRand(0.0f, 1.0f)));  // Polar angle
+        float theta = glm::acos(glm::pow(1 - glm::linearRand(0.0f, 1.0f), 1 / (1 + m)));  // Polar angle
         float phi = glm::linearRand(0.0f, 2.0f * static_cast<float>(M_PI));       // Azimuthal angle
 
         // Convert spherical coordinates to Cartesian coordinates in the local tangent space
@@ -40,12 +43,13 @@ public:
     }
 
 
-    glm::vec3 computeSoftShadow(const glm::vec3 &point, const glm::vec3 &normal, const std::vector<Object*>& objects) {
+    glm::vec3 computeSoftShadow(const glm::vec3 &point, const glm::vec3 &normal, const std::vector<Object *> &objects) {
         int blockedRays = 0;
 
+#pragma omp parallel for schedule(dynamic, 1)
         for (int i = 0; i < numSamples; ++i) {
             // Generate random direction within a hemisphere
-            glm::vec3 randomDir = generateRandomDirectionInHemisphere(normal);
+            glm::vec3 randomDir = randomDirection(normal);
 
             // Shoot shadow ray towards the light source
             Ray ray(point + 0.001f * normal, randomDir);
@@ -57,6 +61,7 @@ public:
                 Hit hit = object->intersect(ray);
                 if (hit.hit && hit.distance < r) {
                     blockedRays++;
+                    break;
                 }
             }
         }
