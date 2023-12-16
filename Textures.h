ad2731207b859dbc1a23cb854e703e557d0f2ab3
Sofia d'Atri <datris@usi.ch>
2023-12-16 21:16:47 +0100

    feat: created opal texture and perlin generated normals
    Signed-off-by: Sofia d'Atri <datris@usi.ch>

--- Textures.h
+++ Textures.h
@@ -8,74 +8,102 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/noise.hpp"
 
+
+glm::vec3 perlinCalculations(glm::vec2 uv, glm::vec3 u_scales, glm::vec3 v_scales, glm::vec3 offsets) {
+
+    float r = glm::perlin(glm::vec3{uv.x * u_scales.r, uv.y * v_scales.r, offsets.r});
+    float g = glm::perlin(glm::vec3{uv.x * u_scales.g, uv.y * v_scales.g, offsets.g});
+    float b = glm::perlin(glm::vec3{uv.x * u_scales.b, uv.y * v_scales.b, offsets.b});
+
+    return {r, g, b};
+}
+
+glm::vec3 perlinNormal(glm::vec2 uv) {
+    glm::vec3 u_scales = glm::vec3(0.0f, 0.0f, 50.f);
+    glm::vec3 v_scales = glm::vec3(0.0f, 0.0f, 50.0f);
+    glm::vec3 offsets = glm::vec3(0.0f, 0.0f, 50.0f);
+
+    // Generate Perlin noise at the given UV coordinates
+    glm::vec3 p = perlinCalculations(uv, u_scales, v_scales, offsets);
+    p = 2.0f * p - 1.0f; // to -1,1
+    p = -glm::normalize(p);
+    p = (p + 1.0f) / 2.0f; // to 0,1
+
+    // Perturb the normal vector based on the Perlin noise value
+    glm::vec3 normal = glm::normalize(glm::vec3(uv, p.z));
+
+    return normal;
+
+
+    /*
+    glm::vec3 u_scales = glm::vec3(25.0f, 20.f, 15.f);
+    glm::vec3 v_scales = glm::vec3(15.0f, 25.0f, 35.0f);
+    glm::vec3 offsets = glm::vec3(0.0f, 0.1f, 0.2f);
+
+    glm::vec3 p = perlinCalculations(uv, u_scales, v_scales, offsets);
+
+    p = 2.0f * p - 1.0f;
+    p = glm::normalize(p);
+    p = (p + 1.0f) / 2.0f;
+    glm::vec3 base = glm::vec3(0.5f, 0.5f, 1.0f);
+
+
+    float r = 0.5f + p.r;
+    float g = 0.5f + p.g;
+    float b = 1.0f + p.b;
+    glm::vec3 color = glm::vec3(r, g, b);
+    color = 2.0f * color - 1.0f;
+    color = glm::normalize(color);
+    color = (color + 1.0f) / 2.0f;
+
+    return color;*/
+
+}
+
+glm::vec3 opal(glm::vec2 uv) {
+    glm::vec3 u_scales = glm::vec3(20.0f, 10.f, 15.0f);
+    glm::vec3 v_scales = glm::vec3(20.0f);
+    glm::vec3 offsets = glm::vec3(0.2f, 0.3f, 0.4f);
+
+    glm::vec3 p = perlinCalculations(uv, u_scales, v_scales, offsets);
+    p = 2.0f * p - 1.0f;
+    p = glm::normalize(p);
+    p = (p + 1.0f) / 2.0f;
+    glm::vec3 base = glm::vec3(0.5f, 0.5f, 1.0f);
+    float r = 0.5f + 0.4f * p.r;
+    float g = 0.5f + 0.3f * p.g;
+    float b = 0.5f + 0.2f * p.b;
+
+
+    glm::vec3 color = glm::normalize(glm::vec3(r, g, b));
+
+    return {r, g, b};
+
+}
+
 glm::vec3 perlinTerrain(glm::vec2 uv) {
-    glm::vec3 u_scales = glm::vec3(30.0f);
-    glm::vec3 v_scales = glm::vec3(30.0f);
-    glm::vec3 offsets = glm::vec3(0.0f);
+    glm::vec3 p = perlinCalculations(uv, glm::vec3(30.0f), glm::vec3(30.0f), glm::vec3(0.0f));
 
-    float r = glm::perlin(glm::vec2{uv.x * u_scales.r, uv.y * v_scales.r + offsets.r});
-    float g = glm::perlin(glm::vec2{uv.x * u_scales.g, uv.y * v_scales.g + offsets.g});
-    float b = glm::perlin(glm::vec2{uv.x * u_scales.b, uv.y * v_scales.b + offsets.b});
+    float r = 0.6f + 0.4f * p.r;
+    float g = 0.4f + 0.3f * p.g;
+    float b = 0.2f + 0.2f * p.b;
 
-    float brownR = 0.6f + 0.4f * r; // Scale towards brown-red
-    float brownG = 0.4f + 0.3f * g; // Scale towards brown-green
-    float brownB = 0.2f + 0.2f * b; // Scale towards brown-blue
-
-    return {brownR, brownG, brownB};
+    return {r, g, b};
 }
 
 glm::vec3 perlinIceTerrain(glm::vec2 uv) {
-    glm::vec3 u_scales = glm::vec3(10);
-    glm::vec3 v_scales = glm::vec3(10);
-    glm::vec3 offsets = glm::vec3(10);
 
-    glm::vec3 v;
-
-    v.r = glm::perlin(glm::vec2{(uv.x) * u_scales.r, uv.y * v_scales.r + offsets.r});
-    v.g = glm::perlin(glm::vec2{(uv.x) * u_scales.g, uv.y * v_scales.g + offsets.g});
-    v.b = glm::perlin(glm::vec2{(uv.x) * u_scales.b, uv.y * v_scales.b + offsets.b});
+    glm::vec3 p = perlinCalculations(uv, glm::vec3(10.0f), glm::vec3(10.0f), glm::vec3(10.0f));
 
     glm::vec3 col1_rgb = glm::vec3(0.722, 0.961, 0.937);
     glm::vec3 col2_rgb = glm::vec3(0.075, 0.482, 0.631);
-    glm::vec3 result;
-    result.r = v.r * col1_rgb.r + ((1.0f - v.r) * col2_rgb.r);
-    result.g = v.g * col1_rgb.g + ((1.0f - v.g) * col2_rgb.g);
-    result.b = v.b * col1_rgb.b + ((1.0f - v.b) * col2_rgb.b);
 
-    return result;
+    float r = p.r * col1_rgb.r + ((1.0f - p.r) * col2_rgb.r);
+    float g = p.g * col1_rgb.g + ((1.0f - p.g) * col2_rgb.g);
+    float b = p.b * col1_rgb.b + ((1.0f - p.b) * col2_rgb.b);
+
+    return {r, g, b};
 }
 
-class ImageTexture {
-protected:
-    image::BMPMini color;
-    image::BMPMini normal;
-    image::BMPMini ambient_occlusion;
-    image::BMPMini roughness;
-
-private:
-    int width;
-    int height;
-
-    static image::BMPMini load_image(const std::string& image_path) {
-        image::BMPMini bmp;
-        bmp.read(image_path);
-        return bmp;
-    }
-public:
-    glm::vec3 getPixel(const glm::vec2 &uv) const {
-        const size_t x = width * std::fmod(uv.x * horizontalScale_, 1);
-        const size_t y = height - height * std::fmod(uv.y * verticalScale_, 1);
-
-        const size_t index = channels * (width * y + x);
-        return glm::vec3{
-                image.data[index + 2],
-                image.data[index + 1],
-                image.data[index]
-        } / 255.f;
-    }
-};
-
-    }
-};
 
 #endif /* Textures_h */
