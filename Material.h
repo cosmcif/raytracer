f1de4c32738efc4817a3c80bf712b604e91978e3
Sofia d'Atri <datris@usi.ch>
2023-12-17 01:39:36 +0100

    feat: added normal map
    Signed-off-by: Sofia d'Atri <datris@usi.ch>

--- Material.h
+++ Material.h
@@ -12,16 +12,22 @@
  Structure describing a material of an object
  */
 struct Material {
-  glm::vec3 ambient = glm::vec3(0.0);
-  glm::vec3 diffuse = glm::vec3(1.0);
-  glm::vec3 specular = glm::vec3(0.0);
+    glm::vec3 ambient = glm::vec3(0.0);
+    glm::vec3 diffuse = glm::vec3(1.0);
+    glm::vec3 specular = glm::vec3(0.0);
 
-  float reflection = 0.0f;
-  float refraction = 0.0f;
-  float sigma = 1.0f;
+    float reflection = 0.0f;
+    float refraction = 0.0f;
+    float sigma = 1.0f;
 
-  float shininess = 0.0;
-  glm::vec3 (*texture)(glm::vec2 uv) = nullptr;
+    float shininess = 0.0f;
+
+    glm::vec3 (*texture)(glm::vec2 uv) = nullptr;
+
+    bool hasNormalMap = false;
+
+    glm::vec3 (*normalMap)(glm::vec2 uv) = nullptr;
+
 };
 
 #endif /* Material_h */
